2f0bb97e1476ef259580eb176757247b0636b146
Afrin Ayesha <shamsafrinayesha02@gmail.com>
2021-03-22 11:37:32 +0530

    WIP: refresh token

--- main.py
+++ main.py
@@ -1,27 +1,92 @@
+from datetime import timedelta
+
 from flask import Flask
 from flask_restful import Api
-from flask_jwt_extended import jwt_manager
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
+from blacklist import BLACKLIST
+from models.user import UserModel
 from resources.agent import AgentsList, AgentRegister
 from resources.policy import Policy
-from resources.user import Users
+from resources.user import Users, UserLogin, UserLogout, TokenRefresh
 from resources.customer import CustomerList, Customer, CustomerRegister
 from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=15)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(hours=1)
 app.secret_key = 'data_service_key'
+CORS(app)
 
 api = Api(app)
-# jwt = jwt_manager(app)
+
 
 @app.before_first_request
 def create_tables():
-    print('creatingngg')
+    print('creating')
     db.create_all()
 
 
+jwt = JWTManager(app)
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blacklist(jwt_header, decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST
+
+@jwt.additional_claims_loader
+def add_claims_to_jwt(identity):
+    user = UserModel.find_by_id(identity)
+    return {'userType': user.usertype}
+
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return {
+        'message': 'The token has expired.',
+        'error': 'token_expired'
+    }, 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return {
+        'message': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }, 401
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return {
+        "description": "Request does not contain an access token.",
+        'error': 'authorization_required'
+    }, 401
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return {
+        "description": "The token is not fresh.",
+        'error': 'fresh_token_required'
+    }, 401
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return {
+        "description": "The token has been revoked.",
+        'error': 'token_revoked'
+    }, 401
+
+
 api.add_resource(Users, '/users')
+api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
+api.add_resource(TokenRefresh, '/refresh')
 api.add_resource(AgentRegister, '/register/agent')
 api.add_resource(AgentsList, '/agents')
 api.add_resource(CustomerRegister, '/register/customer')
--- resources/user.py
+++ resources/user.py
@@ -1,9 +1,11 @@
-from flask_restful import Resource
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required,
+                                get_jwt, get_jwt_identity)
+from flask_restful import Resource, reqparse
+from werkzeug.security import safe_str_cmp
 
-
+# from main import BLACKLIST
+from blacklist import BLACKLIST
 from models.user import UserModel
-
-
 
 
 class Users(Resource):
@@ -12,3 +14,35 @@ class Users(Resource):
         return {'users': [user.json() for user in users]}, 200
 
 
+class UserLogin(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str, required=True, help='username required')
+    parser.add_argument('password', type=str, required=True, help='password required')
+
+    def post(self):
+        data = UserLogin.parser.parse_args()
+        user = UserModel.find_by_username(data['username'])
+        if user and safe_str_cmp(user.password, data['password']):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }
+        return {'message': 'Invalid credentials'}, 401
+
+
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_jwt()['jti']
+        BLACKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
+
+
+class TokenRefresh(Resource):
+    @jwt_required(refresh=True)
+    def get(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token}, 200
